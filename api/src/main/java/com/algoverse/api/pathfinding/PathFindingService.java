4d87dc0b956e5688ac1ea22ce9e7d60cdcd684c3
Julius <julius.brehme@googlemail.com>
2023-10-03 16:44:25 +0900

    added method for pathfinding

--- api/src/main/java/com/algoverse/api/pathfinding/PathFindingService.java
+++ api/src/main/java/com/algoverse/api/pathfinding/PathFindingService.java
@@ -1,15 +1,37 @@
 package com.algoverse.api.pathfinding;
 
 import com.algoverse.api.pathfinding.board.BoardInformation;
+import com.algoverse.api.pathfinding.board.Coordinates;
+import com.algoverse.api.pathfinding.strategy.Dijkstra;
 import com.algoverse.api.pathfinding.strategy.Path;
 import com.algoverse.api.pathfinding.strategy.Strategies;
-import java.util.Arrays;
+import com.algoverse.api.pathfinding.strategy.Strategy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PathFindingService {
 
+  private Strategy strategy;
+
+  public void setStrategy(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
   public Path findPath(BoardInformation board, Strategies strategies) {
+    if (strategies == Strategies.DIJKSTRA) {
+      setStrategy(new Dijkstra());
+    }
+    return strategy.findPath(board);
+  }
+
+
+  // TODO: implement a random nodes generator
+  public Coordinates[] randomNodeGenerator(Coordinates boardSize) {
+    return null;
+  }
+
+  // TODO: implement a random board generator
+  public BoardInformation randomBoardGenerator(Coordinates boardSize) {
     return null;
   }
 
