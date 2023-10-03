7f5fe01848808ecadc2f0b27f7e36ebb6a2be57b
Julius <julius.brehme@googlemail.com>
2023-10-03 16:43:59 +0900

    added exception for points not on board

--- api/src/main/java/com/algoverse/api/pathfinding/PathFindingController.java
+++ api/src/main/java/com/algoverse/api/pathfinding/PathFindingController.java
@@ -5,7 +5,7 @@ import com.algoverse.api.pathfinding.board.BoardInformation;
 import com.algoverse.api.pathfinding.board.Coordinates;
 import com.algoverse.api.pathfinding.strategy.Path;
 import com.algoverse.api.pathfinding.strategy.Strategies;
-import java.util.Arrays;
+import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,45 +21,53 @@ public class PathFindingController {
     this.pathFindingService = pathFindingService;
   }
 
+  // TODO: implement a random nodes generator, s. PathFindingService
   @GetMapping("/pathfinding/random/nodes")
   public Coordinates[] createRandomNodes(@RequestParam(value = "size") int[] boardSize) {
     if (boardSize.length != 2) {
       throw new ExceptionHandling.WrongCoordinateInputException();
     }
-    return null;
+    return pathFindingService.randomNodeGenerator(new Coordinates(boardSize[0], boardSize[1]));
   }
 
+  // TODO: implement a random board generator, s. PathFindingService
   @GetMapping("pathfinding/random/board")
   public BoardInformation createRandomBoard(@RequestParam(value = "size") int[] boardSize) {
     if (boardSize.length != 2) {
       throw new ExceptionHandling.WrongCoordinateInputException();
     }
-    return null;
+    return pathFindingService.randomBoardGenerator(new Coordinates(boardSize[0], boardSize[1]));
   }
 
   @GetMapping("pathfinding/findpath")
   public Path findPath(@RequestParam(value = "startpoint") int[] startingNode,
                        @RequestParam(value = "endpoint") int[] endingNode,
-                       @RequestParam(value = "wall") int[][] wall,
+                       @RequestParam(value = "wall", required = false, defaultValue = " ") int[][] wall,
                        @RequestParam(value = "size") int[] boardSize,
                        @RequestParam(value = "strategy") Strategies strategy) {
     if (startingNode.length != 2 || endingNode.length != 2 || boardSize.length != 2) {
       throw new ExceptionHandling.WrongCoordinateInputException();
     }
-    System.out.println(Arrays.toString(wall));
-    Coordinates[] obstacles = new Coordinates[wall.length];
-    for (int i = 0; i < wall.length; i++) {
-      if (wall[i].length != 2) {
-        throw new ExceptionHandling.WrongWallInputException();
+    if (startingNode[0] < 0 || startingNode[1] < 0 || startingNode[0] >= boardSize[0] || startingNode[1] >= boardSize[1]
+      || endingNode[0] < 0 || endingNode[1] < 0 || endingNode[0] >= boardSize[0] || endingNode[1] >= boardSize[1]) {
+      throw new ExceptionHandling.WrongInputOfBoardSizeAndPoints();
+    }
+    HashMap<Coordinates, Integer> obstacle = new HashMap<>();
+    //Coordinates[] obstacles = new Coordinates[wall.length];
+    if (!(wall.length == 1 && wall[0].length == 0)) {
+      for (int i = 0; i < wall.length; i++) {
+        if (wall[i].length != 2) {
+          throw new ExceptionHandling.WrongWallInputException();
+        }
+        //obstacles[i] = new Coordinates(wall[i][0], wall[i][1]);
+        obstacle.put(new Coordinates(wall[i][0], wall[i][1]), 1);
       }
-      obstacles[i] = new Coordinates(wall[i][0], wall[i][1]);
     }
     return pathFindingService.findPath(
         new BoardInformation(new Coordinates(startingNode[0], startingNode[1]),
             new Coordinates(endingNode[0], endingNode[1]),
-            obstacles,
+            obstacle,
             new Coordinates(boardSize[0], boardSize[1])),
             strategy);
   }
-
 }
