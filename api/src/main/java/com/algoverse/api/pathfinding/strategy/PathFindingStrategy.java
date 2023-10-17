4e3b4adf0c48f04fbdbdad370e6c33a9ce2e6cbf
Julius <julius.brehme@googlemail.com>
2023-10-17 22:10:16 +0900

    added getNeigbors method in interface, becauses needed for every path finding algorithm

--- api/src/main/java/com/algoverse/api/pathfinding/strategy/PathFindingStrategy.java
+++ api/src/main/java/com/algoverse/api/pathfinding/strategy/PathFindingStrategy.java
@@ -1,6 +1,11 @@
 package com.algoverse.api.pathfinding.strategy;
 
 import com.algoverse.api.pathfinding.board.BoardInformation;
+import com.algoverse.api.pathfinding.board.Coordinates;
+import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * The interface for the Strategy. Every new implemented path finding strategy
@@ -15,5 +20,53 @@ public interface PathFindingStrategy {
    * @return Returns a record of Path, which holds the path and the visited nodes
    */
   Path findPath(BoardInformation board);
+
+  /**
+   * This method is to generate the next neighbors for the path finding algorithm.
+   *
+   * @param node         The node to generate the neighbors from
+   * @param wall         All obstacles, those nodes can not be visited or used
+   * @param visitedNodes All already visited nodes
+   * @param boardSize    The size of the board
+   * @return Returns a list of all neighbors, that still need to be visited
+   */
+  default List<Coordinates> getNeighbors(Coordinates node, ImmutableMap<Coordinates, Integer> wall,
+                                        HashMap<Coordinates, Integer> visitedNodes,
+                                        Coordinates boardSize) {
+    // if we allow weighted nodes, implement getNeighbors with a priorityQueue (heap),
+    // so that we use a priority as an attribute getNeighbors updates that priorityQueue.
+    // Changes need to be made to findPath as well
+
+    List<Coordinates> neighbors = new ArrayList<>();
+    if (0 <= node.x() + 1 && node.x() + 1 < boardSize.x() && 0 <= node.y()
+        && node.y() < boardSize.y()) {
+      Coordinates neighbor = new Coordinates(node.x() + 1, node.y());
+      if (wall.get(neighbor) == null && visitedNodes.get(neighbor) == null) {
+        neighbors.add(neighbor);
+      }
+    }
+    if (0 <= node.x() && node.x() < boardSize.x() && 0 <= node.y() + 1
+        && node.y() + 1 < boardSize.y()) {
+      Coordinates neighbor = new Coordinates(node.x(), node.y() + 1);
+      if (wall.get(neighbor) == null && visitedNodes.get(neighbor) == null) {
+        neighbors.add(neighbor);
+      }
+    }
+    if (0 <= node.x() - 1 && node.x() - 1 < boardSize.x() && 0 <= node.y()
+        && node.y() < boardSize.y()) {
+      Coordinates neighbor = new Coordinates(node.x() - 1, node.y());
+      if (wall.get(neighbor) == null && visitedNodes.get(neighbor) == null) {
+        neighbors.add(neighbor);
+      }
+    }
+    if (0 <= node.x() && node.x() < boardSize.x() && 0 <= node.y() - 1
+        && node.y() - 1 < boardSize.y()) {
+      Coordinates neighbor = new Coordinates(node.x(), node.y() - 1);
+      if (wall.get(neighbor) == null && visitedNodes.get(neighbor) == null) {
+        neighbors.add(neighbor);
+      }
+    }
+    return neighbors;
+  }
 
 }
