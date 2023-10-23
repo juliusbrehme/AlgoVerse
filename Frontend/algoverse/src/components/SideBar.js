5787f013874bba829b45bfb6c41101b8da460afe
JarUml <jarodumland@gmail.com>
2023-10-23 22:59:19 +0900

    Toggle button for Sidebar

--- Frontend/algoverse/src/components/SideBar.js
+++ Frontend/algoverse/src/components/SideBar.js
@@ -5,78 +5,51 @@ import { Link } from 'react-router-dom';
 function SideBar() {  
 
   const openDropDown = () => {
+    alert("I'm an alert");
   }
+
   return (
-    <>
     <div className="container">
-      <div className="side-bar" >
+      <nav className="side-bar">
         <Logo />
-        <div className="algo-container">
-          <nav className="algo-nav">
-            <ul className="algo-list">
-              <li className="algo-items" onClick={openDropDown()}>
-              <div className="algo-side-icon">
-                  <img 
-                  src="images/PathFinding-img.png"
-                  alt="pathfinding-icon"
-                  height="30px"
-                  width="30px"
-                  className="path-icon"/>
-                </div>
-                <Link to="/Pathfinding">Pathfinding</Link>
-                <div className="arrow-container">
-                  <img 
-                  src="images/ArrowDown.png"
-                  alt="arrowdown"
-                  height="30px"
-                  width="30px"
-                  className="expand-arrow"></img>
-                </div>
-              </li>
-              <li className="algo-items">
-                <div className="algo-side-icon">
-                  <img 
-                  src="images/Sorting-img.png"
-                  alt="sort-icon"
-                  height="30px"
-                  width="30px"
-                  className="sort-icon" />
-                </div>
-                <Link to="/Sort">Sorting</Link>
-                <div className="arrow-container">
-                  <img 
-                  src="images/ArrowDown.png"
-                  alt="arrowdown"
-                  height="30px"
-                  width="30px"
-                  className="expand-arrow"></img>
-                </div>
-              </li>
-              <li className="algo-items">
-                <div className="algo-side-icon-tree">
-                  <img 
-                  src="images/TreeSearch-img.png"
-                  alt="treesearch-icon"
-                  height="40px"
-                  width="30px"
-                  className="tree-icon" />
-                </div>
-                <Link to="/Tree">Trees</Link>
-                <div className="arrow-container">
-                  <img 
-                  src="images/ArrowDown.png"
-                  alt="arrowdown"
-                  height="30px"
-                  width="30px"
-                  className="expand-arrow"/>
-                </div>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+        <button className="menu-btn">
+            <div className="menu-icon">
+                <div className="menu-line"></div>
+                <div className="menu-line"></div>
+                <div className="menu-line"></div>
+            </div>
+        </button>
+        <ul className="algo-list">
+          <li className="algo-items">
+            <div className="algo-side-icon">
+              <img src="images/PathFinding-img.png" alt="pathfinding-icon" />
+            </div>
+            <Link to="/Pathfinding">Pathfinding</Link>
+            <div className="arrow-container" onClick={openDropDown}>
+              <i className="arrow down"></i>
+            </div>
+          </li>
+          <li className="algo-items">
+            <div className="algo-side-icon">
+              <img src="images/Sorting-img.png" alt="sort-icon" />
+            </div>
+            <Link to="/Sort">Sorting</Link>
+            <div className="arrow-container">
+              <i className="arrow down"></i>
+            </div>
+          </li>
+          <li className="algo-items">
+            <div className="algo-side-icon tree">
+              <img src="images/TreeSearch-img.png" alt="treesearch-icon" />
+            </div>
+            <Link to="/Tree">Trees</Link>
+            <div className="arrow-container">
+              <i className="arrow down"></i>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
-    </>
   );
 }
 
