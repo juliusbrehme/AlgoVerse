bd87accc7ccb501ba312584a5356ebe1715647bf
ruizdiasmatt <m_d_125@hotmail.com>
2023-12-05 22:58:42 +0900

    Applications component added to sorting algorithms

--- Frontend/algoverse/src/components/pages/Sort.js
+++ Frontend/algoverse/src/components/pages/Sort.js
@@ -1,6 +1,7 @@
 import "../../App.css";
 import "./Sort.css";
 import React from "react";
+import Applications from "../infoContainer/Applications";
 
 import SortingVisualizer from "../SortingVisualizer/SortingVisualizer";
 
@@ -11,6 +12,9 @@ function Sort() {
         <div className="buttonHeader"></div>
         <div className="cutBody">
           <SortingVisualizer></SortingVisualizer>
+          <div className="infoContainer">
+            <Applications></Applications>
+          </div>
         </div>
       </div>
     </div>
