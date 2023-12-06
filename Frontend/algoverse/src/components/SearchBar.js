1c9e30abe843c59a050a1649e2ea1a7dd8841186
yunnniverse <dbstjd0726@naver.com>
2023-12-06 16:10:24 +0900

    search bar - tree add

--- Frontend/algoverse/src/components/SearchBar.js
+++ Frontend/algoverse/src/components/SearchBar.js
@@ -9,6 +9,7 @@ const pages = [
   { name: "BFS", path: "/pathfinding?algorithm=BFS" },
   { name: "DFS", path: "/pathfinding?algorithm=DFS" },
   { name: "A*", path: "/pathfinding?algorithm=AStar" },
+  { name: "Binary Search Tree", path: "/Tree?algorithm=BinaryTree" },
   // other pages
 ];
 
